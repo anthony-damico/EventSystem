86f3e9875f27e828bce79881f9be75a48de98b45
anthony-damico <52482785+anthony-damico@users.noreply.github.com>
2020-05-21 09:35:31 +1000

    Initial commit

---
+++ Assets/ButtonTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+    public void PressedLeftClick()
+    {
+        Debug.Log("You Left Clicked " + this.name);
+    }
+
+    public void PressedMiddleClick()
+    {
+        Debug.Log("You Middle Clicked " + this.name);
+    }
+
+    public void PressedRightClick()
+    {
+        Debug.Log("You Right Clicked " + this.name);
+    }
+
+    public void PressedSubmit()
+    {
+        Debug.Log("You Pressed Submit " + this.name);
+    }
+
+    public void PressedCancel()
+    {
+        Debug.Log("You Pressed Cancel " + this.name);
+    }
+}
---
+++ Assets/RightClick.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class RightClick : MonoBehaviour, IPointerClickHandler
+{
+
+    public UnityEvent leftClick;
+    public UnityEvent middleClick;
+    public UnityEvent rightClick;
+    public UnityEvent submit;
+    public UnityEvent cancel;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+            leftClick.Invoke();
+        else if (eventData.button == PointerEventData.InputButton.Middle)
+            middleClick.Invoke();
+        else if (eventData.button == PointerEventData.InputButton.Right)
+            rightClick.Invoke();
+    }
+
+    public void Update()
+    {
+        if(Input.GetButton("Submit"))
+        {
+            submit.Invoke();
+        }
+        else if (Input.GetButton("Cancel"))
+        {
+            cancel.Invoke();
+        }
+    }
+}
